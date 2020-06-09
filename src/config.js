5eee61659c7320c218fcc9aa922a39f6dde0523b
Jae Kim <jckim81@yahoo.com>
2020-06-09 13:17:56 -0400

    new branch implement-your-mvp

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/'
 }
 
 
--- src/server.js
+++ src/server.js
@@ -7,3 +7,5 @@ const { PORT } = require('./config')
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
+
+
