eb89bf8ded4d1469d950893867da652beb3773c9
Jae Kim <jckim81@yahoo.com>
2020-06-26 17:11:02 -0400

    all tests passing at end of secure login implementation

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -20,7 +20,7 @@ function requireAuth(req, res, next) {
             .then(user => {
                 if (!user)
                     return res.status(401).json({ error: 'Unauthorized request' })
-
+            req.user = user
             next()
         })
         .catch(err => {
