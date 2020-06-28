816227d1a207334d2249bece60a377c814ae15ca
Jae Kim <jckim81@yahoo.com>
2020-06-27 21:39:52 -0400

    modified config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://jaekim@localhost/nomnom',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '60m',
 }
 
 
